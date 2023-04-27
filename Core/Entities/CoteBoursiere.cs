c9ed37bdc7115766faa808418dfe73e6d767aa85
WilliamDuK <william.duquet@videotron.ca>
2023-04-27 15:01:04 -0400

    Fix

--- Core/Entities/CoteBoursiere.cs
+++ Core/Entities/CoteBoursiere.cs
@@ -6,6 +6,8 @@ public class CoteBoursiere
 {
     [Key]
     public long id { get; set; }
+
     public string? Titre { get; set; }
+    
     public string? Description { get; set; }
 }
--- Dtos/CoteBoursiereDto.cs
+++ Dtos/CoteBoursiereDto.cs
@@ -3,5 +3,6 @@ namespace EvolutionBoursiere.Dtos;
 public class CoteBoursiereDto
 {
     public string? Titre { get; set; }
+    
     public string? Description { get; set; }
 }
