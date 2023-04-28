91dfddb4ee3a05c830fe291a7271d7022df46648
WilliamDuK <william.duquet@videotron.ca>
2023-04-28 15:19:38 -0400

    Enlever les using inutiles

--- Controllers/HttpRequeteController.cs
+++ Controllers/HttpRequeteController.cs
@@ -1,15 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using EvolutionBoursiere.Infrastructure.Data;
 using EvolutionBoursiere.Core.Entities;
-using EvolutionBoursiere.Dtos;
-using MongoDB.Bson;
-using MongoDB.Driver;
 using EvolutionBoursiere.Infrastructure.Services;
 
 namespace EvolutionBoursiere.Controllers
